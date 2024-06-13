ece6550ae3b94a5447a7c5a7084465b4c6d67b96
BRVM-infos <alfreddiokou274@gmail.com>
2024-06-13 16:46:17 +0200

    Update2 app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def main():
     st.title("Power BI Report")
     st.markdown(
         """
-       <iframe title="Research BRVM" width="700" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=1e3d519f-1117-4b8b-8edb-887aac0ca4a3&autoAuth=true&ctid=ae3bf4c3-6bdd-4312-914f-b6e938c1c344" frameborder="0" allowFullScreen="true"></iframe>
+       <iframe title="Research BRVM" width="1000" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=1e3d519f-1117-4b8b-8edb-887aac0ca4a3&autoAuth=true&ctid=ae3bf4c3-6bdd-4312-914f-b6e938c1c344" frameborder="0" allowFullScreen="true"></iframe>
         """
         , unsafe_allow_html=True
     )
