2bba65e1037e8ad2686c378c95f4713a5fbdbbb3
BRVM-infos <alfreddiokou274@gmail.com>
2024-06-15 13:35:47 +0200

    Update AppProject.py

--- AppProject.py
+++ AppProject.py
@@ -5,27 +5,11 @@ import streamlit as st
 import requests
 # Get the path to the CSV file on Google Drive
 #csv_path = "/content/BRVM"
-csv_url = 'https://github.com/BRVM-infos/Report-App/blob/main/BRVM.csv'
-def load_data(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Vérifier les erreurs de requête
-        csv_content = response.content
-        df = pd.read_csv(pd.compat.StringIO(csv_content))
-        return df
-    except requests.exceptions.RequestException as e:
-        st.error(f"Erreur lors de la requête HTTP : {e}")
-        return None
-    except pd.errors.ParserError as e:
-        st.error(f"Erreur lors de la lecture du fichier CSV : {e}")
-        return None
+
 
 st.title('Mon Application Streamlit')
 st.write('Chargement des données depuis GitHub...')
-df = load_data(csv_url)
+df = pd.read_csv("BRVM.csv")
+st.write('Voici les premières lignes du fichier CSV :')
+st.dataframe(df.head())
 
-if df is not None:
-    st.write('Voici les premières lignes du fichier CSV :')
-    st.dataframe(df.head())
-else:
-    st.error('Impossible de charger les données.')
