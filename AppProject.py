a2f9f66a2ddc775b58ea65d09c5fc77a29085b30
BRVM-infos <alfreddiokou274@gmail.com>
2024-06-15 13:09:47 +0200

    Update AppProject.py

--- AppProject.py
+++ AppProject.py
@@ -2,13 +2,20 @@
 
 import pandas as pd
 import streamlit as st
-
+import requests
 # Get the path to the CSV file on Google Drive
 #csv_path = "/content/BRVM"
-
+url_csv = "https://github.com/BRVM-infos/Report-App/blob/main/BRVM.csv"
+def load_data(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Vérifier les erreurs de requête
+        csv_content = response.content
+        df = pd.read_csv(pd.compat.StringIO(csv_content.decode('utf-8')))
+        return df
 
 # Read the CSV file into a Pandas DataFrame
-df = pd.read_csv('BRVM_csv')
+#df = pd.read_csv('BRVM_csv')
 
 # Print the DataFrame
 #df.head()
