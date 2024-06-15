3a2419877ee4476f5bef70c3d5d2ccc54f18b573
BRVM-infos <alfreddiokou274@gmail.com>
2024-06-15 13:15:15 +0200

    Update AppProject.py

--- AppProject.py
+++ AppProject.py
@@ -14,49 +14,7 @@ def load_data(url):
         df = pd.read_csv(pd.compat.StringIO(csv_content.decode('utf-8')))
         return df
 
-# Read the CSV file into a Pandas DataFrame
-#df = pd.read_csv('BRVM_csv')
+st.dataframe(df)
 
-# Print the DataFrame
-#df.head()
-# Créer le graphique en barres pour les dividendes
-def create_bar_chart(dividendes):
-    years = list(dividendes.keys())
-    values = list(dividendes.values())
-
-    fig, ax = plt.subplots()
-    ax.bar(years, values, color=['brown', 'orange', 'yellow', 'purple', 'magenta'])
-    ax.set_xlabel('Année')
-    ax.set_ylabel('Dividende en FCFA')
-    ax.set_title('Dividende en FCFA')
-    return fig
-
-
-def main():
-    st.title('Analyse Financière des Entreprises')
-    st.markdown('<style>body{background-color: lightgrey;}</style>', unsafe_allow_html=True)
-
-    data = df
-    companies = data['Ticket'].tolist()
-
-    st.write("### Sélectionnez une entreprise :")
-    cols = st.columns(len(companies))
-
-    selected_company = st.session_state.get('selected_company', companies[0])
-    
-    # this loop display each company logo once and allow to store eacht company Ticket once
-    for i, company in enumerate(companies):
-        with cols[i]:
-            if st.button(company):
-                st.session_state['selected_company'] = company
-                selected_company = company
-            logo_url = data[data['Ticket'] == company]['Profile.Images'].values[0]
-            st.image(logo_url, width=100)
-
-    st.write(f"## Données pour {selected_company}")
-  #Store each company Ticke once
-    company_data = data[data['Ticket'] == selected_company].iloc[0]
-
-        st.pyplot(create_bar_chart(company_data['Dividende']))
    
-main()
+
