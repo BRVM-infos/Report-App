010f5af834819cf0e416b9e9916fbf3def2fa2fc
BRVM-infos <alfreddiokou274@gmail.com>
2024-06-15 13:58:39 +0200

    Update AppProject.py

--- AppProject.py
+++ AppProject.py
@@ -7,14 +7,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 # Get the path to the CSV file on Google Drive
 #csv_path = "/content/BRVM"
-
-
 st.title('Mon Application Streamlit')
 st.write('Chargement des données depuis GitHub...')
 df = pd.read_csv("BRVM.csv")
 st.write('Voici contenu du fichier CSV :')
 st.dataframe(df)
-
 st.bar_chart(df, x="Date", y="Dividende")
 
 
