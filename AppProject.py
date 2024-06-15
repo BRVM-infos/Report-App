fe81c91fa9d10a0d7c73d115cbd0cde4ad25207c
BRVM-infos <alfreddiokou274@gmail.com>
2024-06-15 13:20:59 +0200

    Update AppProject.py

--- AppProject.py
+++ AppProject.py
@@ -6,8 +6,3 @@ import requests
 # Get the path to the CSV file on Google Drive
 #csv_path = "/content/BRVM"
 csv_url = "https://github.com/BRVM-infos/Report-App/blob/main/BRVM.csv"
-response = requests.get(csv_url)
-csv_content = response.content
-
-# Utiliser pandas pour lire le contenu CSV
-df = pd.read_csv(pd.compat.StringIO(csv_content.decode('utf-8')))
