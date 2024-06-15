3502bf7c43ab568baf58af71cd9d42b3a8710cd3
BRVM-infos <alfreddiokou274@gmail.com>
2024-06-15 13:28:13 +0200

    Update AppProject.py

--- AppProject.py
+++ AppProject.py
@@ -5,5 +5,5 @@ import streamlit as st
 import requests
 # Get the path to the CSV file on Google Drive
 #csv_path = "/content/BRVM"
-csv_url = "https://github.com/BRVM-infos/Report-App/blob/main/BRVM.csv"
+csv_url = 'https://github.com/BRVM-infos/Report-App/blob/main/BRVM.csv'
 df = pd.read_csv(csv_url)
