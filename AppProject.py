ecbe9b5941fe2d4528a0d8b11b114ba2fd1a8c4c
BRVM-infos <alfreddiokou274@gmail.com>
2024-06-15 14:02:47 +0200

    Update AppProject.py

--- AppProject.py
+++ AppProject.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import streamlit as st
 import requests
+import matplotlib.pyplot as plt
 
 # Get the path to the CSV file on Google Drive
 #csv_path = "/content/BRVM"
@@ -12,4 +13,14 @@ df = pd.read_csv("BRVM.csv")
 st.write('Voici contenu du fichier CSV :')
 st.dataframe(df)
 st.bar_chart(df, x="Date", y="Dividende")
-st.line_chart(df, x="Date", y="Dividende")
+# Créer le graphique en barres pour les dividendes
+def create_bar_chart(dividendes):
+    years = list(dividendes.keys())
+    values = list(dividendes.values())
+
+    fig, ax = plt.subplots()
+    ax.bar(years, values, color=['brown', 'orange', 'yellow', 'purple', 'magenta'])
+    ax.set_xlabel('Année')
+    ax.set_ylabel('Dividende en FCFA')
+    ax.set_title('Dividende en FCFA')
+    return fig
