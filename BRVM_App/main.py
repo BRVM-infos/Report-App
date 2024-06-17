b1b906614dfe411fc6c42f8dc5e6107dd82d7c32
BRVM-infos <alfreddiokou274@gmail.com>
2024-06-17 18:13:25 +0200

    Update1 main.py
    Add some code to fix css issue

--- BRVM_App/main.py
+++ BRVM_App/main.py
@@ -21,8 +21,16 @@ st.set_page_config(
 # Set up css file via fucntion
 # Function to load CSS from a file and inject it into the app
 def load_css(file_name):
-    with open(file_name) as f:
+     if os.path.exists(file_name):
+        with open(file_name) as f:
         st.markdown(f'<style >{f.read()}</style>', unsafe_allow_html=True)
+     else:
+        st.error(f"CSS file '{file_name}' not found. Please check the file path.")
+
+# Set the working directory to the directory containing this script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
+        
 
 # Load the CSS file
 load_css('style.css')
