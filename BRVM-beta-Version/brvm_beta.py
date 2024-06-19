7024ae3081c386fc39dc19a8c405780c8a9a72bf
BRVM-infos <alfreddiokou274@gmail.com>
2024-06-19 09:24:33 +0200

    Update brvm_beta.py
    add css file path

--- BRVM-beta-Version/brvm_beta.py
+++ BRVM-beta-Version/brvm_beta.py
@@ -39,16 +39,22 @@ menu_icon="cast", default_index=0, orientation="horizontal")
 #app
 
 ###################################
-# Mutlipage Setup
 # Set up css file via fucntion
 # Function to load CSS from a file and inject it into the app
 def load_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style    >{f.read()}</style>', unsafe_allow_html=True)
+     if os.path.exists(file_name):
+        with open(file_name) as f:
+            st.markdown(f'<style >{f.read()}</style>', unsafe_allow_html=True)
+     else:
+        st.error(f"CSS file '{file_name}' not found. Please check the file path.")
+
+# Set the working directory to the directory containing this script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
+        
 
 # Load the CSS file
 load_css('style.css')
-
 # Read csv file
 df_main = pd.read_csv('brvm_data.csv')
 
