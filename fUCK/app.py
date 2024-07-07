ab3f1f2c0e1227e0c184ebe4e1587e25316b6927
BRVM-infos <alfreddiokou274@gmail.com>
2024-07-07 06:19:56 +0200

    Update app.py

--- fUCK/app.py
+++ fUCK/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
 #from langchain_community.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
